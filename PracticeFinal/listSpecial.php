c22684b8c6f6cde5021b0cbb283a518921772d52
Zachary Kurmas <kurmasz@users.noreply.github.com>
2019-04-12 15:41:10 -0400

    Initial commit

---
+++ PracticeFinal/listSpecial.php
@@ -0,0 +1,33 @@
+<?php
+$xmlDoc = new DomDocument();
+$xmlDoc->load("input1.html");
+$root = $xmlDoc->documentElement;
+
+
+
+?>
+
+<html>
+<head>
+
+
+</head>
+
+<body>
+<h1>Special Words</h1>
+
+<h2>All Caps</h2>
+<ul>
+
+</ul>
+
+<h2>Upcase</h2>
+<ul>
+
+</ul>
+
+
+
+</body>
+
+</html>
